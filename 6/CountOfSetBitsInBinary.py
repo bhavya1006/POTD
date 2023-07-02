08f7397998e76319cec41538dd48661be41e2b3c
Bhavya Madan <121045636+bhavya1006@users.noreply.github.com>
2023-07-02 20:56:14 +0530

    Add files via upload

---
+++ 6/CountOfSetBitsInBinary.py
@@ -0,0 +1,22 @@
+#User function Template for python3
+class Solution:
+	def setBits(self, N):
+	    return bin(N).count('1')
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__':
+	T=int(input())
+	for i in range(T):
+		N = int(input())
+		ob = Solution()
+		ans = ob.setBits(N)
+		print(ans)
+
+
+
+
+# } Driver Code Ends
